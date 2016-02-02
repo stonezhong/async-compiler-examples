2d28a03b14c2a593c6fe34659d55b92ba8961f0b
Stone Zhong <stonezhong@hotmail.com>
2016-02-02 07:48:05 -0800

    update examples as the compiler and runtime upgrades.

--- abc.js
+++ abc.js
@@ -13,8 +13,19 @@ function foo() {
   return mul(add(3, 4), add(5, 6));
 }
 
+/** @async **/
+function bar() {
+  var sum = 0;
+  for (var i = 0; i <= 10; i ++) {
+    sum = add(sum, i);
+  }
+  return sum;
+}
+
 foo().then(function(result) {
   console.info(result);
 });
 
-
+bar().then(function(result) {
+  console.info(result);
+});
